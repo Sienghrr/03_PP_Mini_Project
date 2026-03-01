84844d4e2c029658afa81151dc7b0841b0eb13b1
Sienghrr <siengsweetheart151@gmail.com>
2026-03-01 13:16:07 +0700

    added : library & create new mapper package for mapping

--- src/dao/ProductDAO.java
+++ src/dao/ProductDAO.java
@@ -2,22 +2,25 @@ package dao;
 
 import domain.Product;
 
+import javax.sound.sampled.Port;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductDAO {
 
     // CRUD
-    boolean insert (Product product);
+    boolean insert(Product product);
     boolean update(Product product);
     boolean delete(int id);
-    Product findById(int id);
+    Optional<Product> findById(int id);
     List<Product> findAll();
 
     // search by productName
-    List<Product> searchByName (String name);
+    Optional<List<Product>> searchByName (String name);
 
     // Pagination
     List<Product> findWithPagination(int offset, int limit);
+    int getTotalCount();
 
     // Save to DB
     boolean saveToDatabase (List<Product> toInsert, List<Product> toUpdate, List<Integer> toDelete);
